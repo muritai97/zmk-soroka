99c8848828f3a8291dc7f090de49e032ea38545f
aroum <aroum24@gmail.com>
2024-10-30 09:04:10 +0300

    Update status_led.c

--- config/boards/shields/soroka/status_led.c
+++ config/boards/shields/soroka/status_led.c
@@ -1,3 +1,43 @@
+#include <zmk/events/activity_state_changed.h>
+#include <zmk/events/ble_active_profile_changed.h>
+#include <zmk/events/usb_conn_state_changed.h>
+#include <zephyr/drivers/led.h>
+
+#include <math.h>
+#include <stdlib.h>
+
+#include <zmk/activity.h>
+#include <zmk/battery.h>
+#include <zmk/ble.h>
+#include <zmk/endpoints.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/battery_state_changed.h>
+
+#include <zmk/hid_indicators.h>
+#include <zmk/keymap.h>
+#include <zmk/usb.h>
+#include <zmk/workqueue.h>
+
+#include <zephyr/bluetooth/services/bas.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/settings/settings.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+// timeout
+#define LED_BLINK_PROFILE_DELAY 200
+#define LED_BLINK_CONN_DELAY 140
+#define LED_FADE_DELAY 2
+#define LED_BATTERY_SHOW_DELAY 700
+#define LED_BATTERY_SLEEP_SHOW 1000
+#define LED_BATTERY_BLINK_DELAY 200
+#define LED_USB_BLINK_DELAY 200
+
+#define LED_STATUS_ON 100
+#define LED_STATUS_OFF 0
 #include <zephyr/device.h>
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
@@ -53,7 +93,13 @@ void hide_battery_work_handler(struct k_work *work) {
     }
     led_strip_update_rgb(led_strip, pixels, STRIP_NUM_PIXELS);
 }
+// Инициализация работ
+void init_battery_leds() {
+    k_work_init_delayable(&show_battery_work, show_battery_work_handler);
+}
 
+// Вызывается при инициализации системы
+SYS_INIT(init_battery_leds, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
 void show_battery() {
     // Планирование работы show_battery через 0 мс
     k_work_schedule(&show_battery_work, K_NO_WAIT);
@@ -64,10 +110,3 @@ void hide_battery() {
     k_work_schedule(&hide_battery_work, K_NO_WAIT);
 }
 
-// Инициализация работ
-void init_battery_leds() {
-    k_work_init_delayable(&show_battery_work, show_battery_work_handler);
-}
-
-// Вызывается при инициализации системы
-SYS_INIT(init_battery_leds, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
